9267001d7f7704818e69ad937fe4c1e5314fc61f
Tekton Pipeline <tekton-pipeline@example.com>
2025-10-16 15:04:04 +0200

    feat: Update greeting to v1.1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const GREETING_MESSAGE = "Hello World v1.0";
+const GREETING_MESSAGE = "Hello World v1.1";
 app.get('/', (req, res) => { res.send(GREETING_MESSAGE); });
 app.listen(port, () => { console.log(`App listening on port ${port}`); });
